b3f68b5e95d5216204efc5c61ed4392b8c069620
nicholasvanlandschoot <nicholasjv17@gmail.com>
2022-06-12 18:31:04 -0600

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,35 @@
 import os
-from rich.console import Console
 import subprocess
 import sys
 
 
-console = Console()
+print("configuring debate-tool")
 
-console.print("configuring debate-tool", style="blue")
+os.system("python3 -m venv venv")
+
+while True:
+    try:
+        os.system("./venv/bin/python3 -m pip install --upgrade pip")
+        os.system("./venv/bin/pip3 install -r requirements.txt")
+        break
+    except:
+        pass
 
 if not os.path.exists("userdata"):
     os.mkdir("userdata")
 
 if not os.path.exists("userdata/config.json"):
-    open("userdata/config.json", "w")
+    with open("userdata/config.json", "w") as f:
+        f.write("{}")
 
 if not os.path.exists("userdata/driveObjects.json"):
-    open("userdata/driveObjects.json", "w")
+    with open("userdata/driveObjects.json", "w") as f:
+        f.write("{}")
 
-console.print("finished config", style="green")
+if not os.path.exists("userdata/snippets.json"):
+    with open("userdata/snippets.json", "w") as f:
+        f.write("{}")
+
+
+print("finished config")
+print("move into vitrual enviroment: source ./venv/bin/activate")
